33c7e78dfdd72a2e565bff69905d5007c4d732b7
chaya <chaya.r.sadler@gmail.com>
2025-11-20 15:30:27 -0600

    code refactoring and fixed query error

--- src/main/java/com.chayasadler.orderservice/dao/IEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chayasadler.orderservice.dao;
-
-import com.chayasadler.orderservice.model.OutBoxEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEventRepository extends JpaRepository<OutBoxEvent, Integer> {
-}
